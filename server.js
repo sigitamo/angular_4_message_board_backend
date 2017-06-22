6dd1b7cf842bca1455bd2b2e718d9ae76006e0de
sigitamocevice <sigita.z@gmail.com>
2017-06-22 14:52:33 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var messages = [
+    { text: 'some text', owner: 'Anderw' },
+    { text: 'some koko text', owner: 'MotoAnderw'},
+    { text: 'some text', owner: 'AlohaAnderw' }
+];
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type,Accept");
+    next();
+})
+
+var api = express.Router();
+
+api.get('/messages',(req, res) => {
+    res.json(messages);
+})
+api.post('/messages',(req, res) => {
+    messages.push(req.body);
+    res.sendStatus(200);
+})
+
+app.use('/api', api);
+
+app.listen(63145);
